a25d2eb0240336a9e1d9ac2f89c6e16c9cc1d0da
danielic2103 <danielic2103@gmail.com>
2017-01-11 14:55:40 +0100

    Add files via upload

--- test.lua
+++ test.lua
@@ -16,12 +16,12 @@ addCommandHandler("testnotify",
 		Notify(
 			{
 				text = texts[math.random(1, #texts)],
-				font = fonts[math.random(1, #fonts)],
+				font = "arial",
 				fontColor = tocolor(255, 255, 255, 255),
 				fontSize = 1.3,
-				height = 35,
-				color = tocolor(255, 0, 80, 255),
-				radius = 5,
+				height = 30,
+				color = tocolor(math.random(0, 255), math.random(0, 255), math.random(0, 255), math.random(150, 255)),
+				radius = 5
 			}
 		)
 	end
