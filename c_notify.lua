c699766b2c7a0ffe938e7fafe990ae7dc8ae0182
danielic2103 <danielic2103@gmail.com>
2017-01-11 14:37:26 +0100

    Add files via upload

---
+++ c_notify.lua
@@ -0,0 +1,33 @@
+﻿local notify = {}
+local screenX, screenY = guiGetScreenSize()
+
+addEventHandler("onClientRender", root,
+	function()
+		local cacheY = 10
+		for i = 1, #notify do
+			local v = notify[i]
+			if v then
+				local width, height = dxGetTextWidth(v[1].text, v[1].fontSize or 1, v[1].font or "default") + 15, v[1].height or 45
+				if v[3] == "appear" then 
+					v[2] = v[2] < 1 and v[2] + 0.15 or 1
+				elseif v[3] == "dissapear" then 
+					if v[2] > 0 then
+						v[2] = v[2] - 0.15
+					else
+						table.remove(notify, i)
+					end
+				end
+				local posX, posY = interpolateBetween(screenX, cacheY-height, 0, (screenX - width)-5, cacheY, 0, v[2], "Linear")
+				_dxDrawRectangle(posX, posY, width, height, v[1].radius or 0, v[1].color, true)
+				dxDrawText(v[1].text, posX, posY, posX+width, posY+height, v[1].fontColor or tocolor(255, 255, 255, 255), v[1].fontSize or 1, v[1].font or "default", "center", "center", false, false, true)
+				cacheY = (cacheY + height) + 5
+			end
+		end
+		if #notify ~= 0 and (getTickCount() - tick) > 1000 then notify[1][3] = "dissapear" tick = getTickCount() end
+	end
+)
+
+function Notify(_table)
+	table.insert(notify, {_table, 0, "appear"})
+	tick = getTickCount()
+end
---
+++ test.lua
@@ -0,0 +1,28 @@
+﻿local texts = {
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"Lorem ipsum dolor sit amet.",
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed egestas leo, nec pharetra mi."
+}
+
+local fonts = {
+	"default",
+	"default-bold",
+	"arial",
+	"clear"
+}
+
+addCommandHandler("testnotify",
+	function()
+		Notify(
+			{
+				text = texts[math.random(1, #texts)],
+				font = fonts[math.random(1, #fonts)],
+				fontColor = tocolor(255, 255, 255, 255),
+				fontSize = 1,
+				height = 25,
+				color = tocolor(255, 0, 80, 255),
+				radius = 5,
+			}
+		)
+	end
+)
